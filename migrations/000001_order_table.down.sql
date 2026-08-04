b7f8f27e8a77ba8625bb18394b73c8fe29264a9a
Dmitry <toknoff@yandex.ru>
2026-08-04 19:03:27 +0300

    fixed migrations

--- migrations/000001_order_table.down.sql
+++ migrations/000001_order_table.down.sql
@@ -1,9 +1,2 @@
-CREATE TABLE IF NOT EXISTS orders (
-    id          BIGSERIAL                PRIMARY KEY,
-    user_id     BIGINT                   NOT NULL,
-    amount      NUMERIC(12, 2)           NOT NULL,
-    status      VARCHAR(32)              NOT NULL DEFAULT 'CREATED',
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX idx_orders_user_id ON orders(user_id);
+DROP INDEX IF EXISTS idx_orders_user_id;
+DROP TABLE IF EXISTS orders;
--- migrations/000002_delivery_table.up.sql
+++ migrations/000002_delivery_table.up.sql
@@ -1,2 +1,9 @@
-DROP INDEX IF EXISTS idx_deliveries_order_id;
-DROP TABLE IF EXISTS deliveries;
+CREATE TABLE IF NOT EXISTS deliveries (
+                                          id           BIGSERIAL       PRIMARY KEY,
+                                          order_id     BIGINT          NOT NULL UNIQUE,
+                                          address      VARCHAR(255)    NOT NULL DEFAULT 'Default',
+    status       VARCHAR(32)     NOT NULL DEFAULT 'PROCESSING',
+    updated_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+                               );
+
+CREATE INDEX idx_deliveries_order_id ON deliveries(order_id);
